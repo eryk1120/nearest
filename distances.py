692e3cc0a5470bb07e4630793d297e15742ac7fb
eryk <eryk1120@gmail.com>
2019-09-20 00:01:26 +0200

    finish?

--- distances.py
+++ distances.py
@@ -1,2 +1,15 @@
 import numpy as np 
 import matplotlib.pyplot as plt
+import seaborn; seaborn.set()
+
+rand = np.random.RandomState(42)
+x = rand.rand(10,2)
+plt.scatter(x[:, 0], x[:, 1], s=100);
+#plt.show()
+dist_sq = np.sum((x[:,np.newaxis,:]-x[np.newaxis,:,:])**2,axis = -1)
+print (dist_sq)
+closest = np.argsort(dist_sq,axis=1)
+print ( closest)
+for i in range(x.shape[0]):
+	plt.plot(*zip(x[i],x[closest[i,1]]),color='blue')
+plt.show()
